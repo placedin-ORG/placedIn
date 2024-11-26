f681cbcaed7b88c17508067f4544c149099cb54b
Mukesh Pandey <panditmukki5@gmail.com>
2024-11-26 10:29:53 +0530

    schema changed to model

--- server/models/courseModel.js
+++ server/models/courseModel.js
@@ -9,6 +9,12 @@ const courseSchema = new mongoose.Schema({
     default: 0,
   },
   title: { type: String, required: true },
+  courseCategory: {
+    type: String,
+  },
+  courseThumbnail: {
+    type: String,
+  },
   description: { type: String },
   status: {
     type: String,
@@ -17,7 +23,7 @@ const courseSchema = new mongoose.Schema({
   },
   chapters: [
     {
-      title: { type: String, required: true },
+      title: { type: String },
       topics: [
         {
           name: { type: String },
@@ -37,12 +43,16 @@ const courseSchema = new mongoose.Schema({
 
   questions: [
     {
-      questionText: { type: String, required: true },
-      options: [{ type: String, required: true }],
-      correctAnswer: { type: String, required: true },
+      questionText: { type: String },
+      options: [{ type: String }],
+      correctAnswer: { type: String },
       image: { type: String }, // Optional for image-based questions
     },
   ],
+  setLive: {
+    type: Boolean,
+    default: false,
+  },
   sponsoredBy: { type: String }, // e.g., 'Google', 'Facebook'
   isSponsored: { type: Boolean, default: false },
 });
